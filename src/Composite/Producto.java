ee37fe9e39dcb8d888cef5c382b61b088970daba
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-11-03 12:20:02 -0600

    Documentacion

--- src/Composite/Producto.java
+++ src/Composite/Producto.java
@@ -1,32 +1,37 @@
 package Composite;
 
 /**
- * Interfaz que define las caracteristicas comunes de los artículos que
- * ofrece la tienda CheemsMart
+ * Interfaz que define los metodos de un producto de la tienda
  *
  * @author paolasanv
  * @author Supr-Lilito
  * @author JesusEVR
- * @version octubre 2023
+ * @version noviembre 2023
  *
  */
 public interface Producto{
-	
 	/**
 	 * Devuelve el precio del artículo
 	 */
 	public double precio();
-	
 	/**
 	 * Devuelve una cadena con la información del artículo
 	 */
 	public String informacion();
-	
+	/**
+	 * Devuelve uuna cadena con la información y el precio del artículo
+	 */
 	public String informacionConPrecio();
-	
+	/**
+	 * Devuelve el codigo unico del producto
+	 */
 	public String codigo();
-	
+	/**
+	 * Devuelve la cantidad de puntos que suma o resta a los puntos de hambre de la mascota
+	 */
 	public double modificaHambre();
-	
+	/**
+	 * Devuelve la cantidad de puntos que suma o resta a los puntos de felicidad de la mascota
+	 */
 	public double modificaFelicidad();
 }
