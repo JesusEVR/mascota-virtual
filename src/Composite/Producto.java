49f18491604e235d5362ce6bdfd3a502d6f82972
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-11-03 13:50:03 -0600

    Documentacion

--- src/Composite/Producto.java
+++ src/Composite/Producto.java
@@ -14,22 +14,27 @@ public interface Producto{
 	 * Devuelve el precio del artículo
 	 */
 	public double precio();
+	
 	/**
 	 * Devuelve una cadena con la información del artículo
 	 */
 	public String informacion();
+	
 	/**
 	 * Devuelve una cadena con la información y el precio del artículo
 	 */
 	public String informacionConPrecio();
+	
 	/**
 	 * Devuelve el codigo unico del producto
 	 */
 	public String codigo();
+	
 	/**
 	 * Devuelve la cantidad de puntos que suma o resta a los puntos de hambre de la mascota
 	 */
 	public double modificaHambre();
+	
 	/**
 	 * Devuelve la cantidad de puntos que suma o resta a los puntos de felicidad de la mascota
 	 */
