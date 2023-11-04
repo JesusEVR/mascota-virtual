bf9135ee0e42917f502d7483e1306e5cab7f91b8
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-11-04 14:56:04 -0600

    Doc

--- src/Prototype/MuertePorAburrimientoException.java
+++ src/Prototype/MuertePorAburrimientoException.java
@@ -1,30 +1,29 @@
 package Prototype;
 
 /**
-* Clase que crea una excepcion para la muerte de una mascota causada por dormir demasiado 
-* o alimentarlo con productos que le resulten aburridos.
-* 
-* @author paolasanv
-* @author Supr-Lilito
-* @author JesusEVR
-* @version octubre 2023
-*
-*/
-
+ * Clase que crea una excepcion para la muerte de una mascota causada por dormir demasiado 
+ * o alimentarlo con productos que le resulten aburridos.
+ * 
+ * @author paolasanv
+ * @author Supr-Lilito
+ * @author JesusEVR
+ * @version noviembre 2023
+ *
+ */
 public class MuertePorAburrimientoException extends Exception{
 	
 	/**
-	* Constructor que recibe como parámetro una cadena
-        *
-	* @param mensaje
-	*/
+	 * Constructor que recibe como parámetro una cadena
+         *
+	 * @param mensaje
+	 */
 	public MuertePorAburrimientoException(String mensaje){
 		super(mensaje);
 	}
 	
 	/**
-	* Constructor por omisión
-	*/
+	 * Constructor por omisión
+	 */
 	public MuertePorAburrimientoException(){
 	}
 
