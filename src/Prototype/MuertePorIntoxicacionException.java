2655ef5d0bad51ca353d932b175bc67cc5a25a9b
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-11-04 14:57:07 -0600

    Doc

--- src/Prototype/MuertePorIntoxicacionException.java
+++ src/Prototype/MuertePorIntoxicacionException.java
@@ -1,28 +1,28 @@
 package Prototype;
 
 /**
-* Clase que crea una excepcion para la muerte de una mascota causada darle alimentos que le resultaron toxicos
-* 
-* @author paolasanv
-* @author Supr-Lilito
-* @author JesusEVR
-* @version octubre 2023
-*
-*/
+ * Clase que crea una excepcion para la muerte de una mascota causada darle alimentos que le resultaron toxicos
+ * 
+ * @author paolasanv
+ * @author Supr-Lilito
+ * @author JesusEVR
+ * @version noviembre 2023
+ *
+ */
 public class MuertePorIntoxicacionException extends Exception{
 	
 	/**
-	* Constructor que recibe como parámetro una cadena
-        *
-	* @param mensaje
-	*/
+	 * Constructor que recibe como parámetro una cadena
+         *
+	 * @param mensaje
+	 */
 	public MuertePorIntoxicacionException(String mensaje){
 		super(mensaje);
 	}
 	
 	/**
-	* Constructor por omisión
-	*/
+	 * Constructor por omisión
+	 */
 	public MuertePorIntoxicacionException(){
 	}
 
