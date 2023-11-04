5b4c60b318f155b54e24a1851c8c0ce44eb86c36
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-11-04 14:57:57 -0600

    Doc

--- src/Prototype/MuertePorNoComerException.java
+++ src/Prototype/MuertePorNoComerException.java
@@ -1,26 +1,27 @@
 package Prototype;
+
 /**
-*Clase que crea una excepcion para la muerte de una mascota causada por jugar demasiado y no darle alimento
-* 
-* @author paolasanv
-* @author Supr-Lilito
-* @author JesusEVR
-* @version octubre 2023
-*/
+ * Clase que crea una excepcion para la muerte de una mascota causada por jugar demasiado y no darle alimento
+ * 
+ * @author paolasanv
+ * @author Supr-Lilito
+ * @author JesusEVR
+ * @version noviembre 2023
+ */
 public class MuertePorNoComerException extends Exception{
 	
 	/**
-	* Constructor que recibe como parámetro una cadena
-        *
-	* @param mensaje
-	*/
+	 * Constructor que recibe como parámetro una cadena
+         *
+	 * @param mensaje
+	 */
 	public MuertePorNoComerException(String mensaje){
 		super(mensaje);
 	}
 	
 	/**
-	* Constructor por omisión
-	*/
+	 * Constructor por omisión
+	 */
 	public MuertePorNoComerException(){
 	}
 
