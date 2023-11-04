ccf81f8cd4b3320d0c998e7aa5cccd17afd5fb6f
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-11-03 18:18:18 -0600

    Documentacion

--- src/Iterator/Catalogo.java
+++ src/Iterator/Catalogo.java
@@ -1,19 +1,18 @@
 package Iterator;
 
 import java.util.Iterator;
+
 /**
-* Interfaz que define la característica común de los catálogos
-*
-*@author paolasanv
-*@author Supr-Lilito
-*@author JesusEVR
-*
-*/
+ * Interfaz que define la característica común de los catálogos
+ *
+ * @author paolasanv
+ * @author Supr-Lilito
+ * @author JesusEVR
+ */
 public interface Catalogo{
 	
 	/**
 	* Crea un iterador sobre el catálogo
-	*
 	*/
 	public Iterator creaIterador();
 }
