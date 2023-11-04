10961fd1db7558245fb639eff972edc8f9c28c96
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-11-04 14:56:38 -0600

    Doc

--- src/Prototype/MuertePorCansancioException.java
+++ src/Prototype/MuertePorCansancioException.java
@@ -1,29 +1,28 @@
 package Prototype;
 
 /**
-* Clase que crea una excepcion para la muerte de una mascota causada por jugar sin dejarlo dormir suficiente
-* 
-* @author paolasanv
-* @author Supr-Lilito
-* @author JesusEVR
-* @version octubre 2023
-*
-*/
-
+ * Clase que crea una excepcion para la muerte de una mascota causada por jugar sin dejarlo dormir suficiente
+ * 
+ * @author paolasanv
+ * @author Supr-Lilito
+ * @author JesusEVR
+ * @version octubre 2023
+ *
+ */
 public class MuertePorCansancioException extends Exception{
 	
 	/**
-	* Constructor que recibe como parámetro una cadena
-        *
-	* @param mensaje
-	*/
+	 * Constructor que recibe como parámetro una cadena
+         *
+	 * @param mensaje
+	 */
 	public MuertePorCansancioException(String mensaje){
 		super(mensaje);
 	}
 	
 	/**
-	* Constructor por omisión
-	*/
+	 * Constructor por omisión
+	 */
 	public MuertePorCansancioException(){
 	}
 
