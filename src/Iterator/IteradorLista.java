5f6a7d28b30c2936d654a411309941c8f6e69630
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-11-03 18:22:56 -0600

    Documentacion

--- src/Iterator/IteradorLista.java
+++ src/Iterator/IteradorLista.java
@@ -2,26 +2,31 @@ package Iterator;
 
 import java.util.LinkedList;
 import java.util.Iterator;
+
 /**
  * Clase que implementa un iterador para lista de artículos del catálogo.
  * 
  * @author Supr-Lilito
  * @author paolasanv
  * @author JesusEVR
- * @version septiembre 2023
+ * @version noviembre 2023
  *
  */
-
 public class IteradorLista implements Iterator{
-
+	/**
+	 * Lista  de productos
+	 */
 	private LinkedList<Object> lista;
+	/**
+	 * Iterador de la lista
+	 */
 	private Iterator it;
-
-    /**
-     * Constructor de la clase IteradorLista.
-     * 
-     * @param lista Lista de productos 
-     */
+	
+	/**
+         * Constructor de la clase IteradorLista.
+         * 
+         * @param lista Lista de productos 
+	 */
 	public IteradorLista(LinkedList<Object> lista){
 		this.lista = lista;
 		it = lista.iterator();
