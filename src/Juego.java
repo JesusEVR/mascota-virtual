02d4cb84932a2750e1da7cd58494b4f240f7c575
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-11-04 18:24:16 -0600

    Update and rename prueba.java to Juego.java

---
+++ src/Juego.java
@@ -0,0 +1,27 @@
+import Composite.Producto;
+import Composite.Alimento;
+import Composite.Inventario;
+import Iterator.Catalogo;
+import Iterator.CatalogoAlimento;
+import Iterator.IteradorLista;
+import java.util.Iterator;
+import Facade.CentroAdopcion;
+
+/**
+ * Clase principal del juego "Juego".
+ *
+ */
+public class Juego{
+	
+	public static void main(String[] args){
+		
+		CentroAdopcion c = new CentroAdopcion();
+		
+		c.darBienvenida();
+		c.elegirNivel();
+		c.adoptar();
+		c.convivir();
+		c.darFelicitacion();
+	
+	}
+}
