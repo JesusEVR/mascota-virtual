54b3d96db4b582d54bbc7708e63157c9b243e4b9
Elias <124617308+JesusEVR@users.noreply.github.com>
2023-11-05 16:34:25 -0600

    Update EstadoMascota.java

--- src/State/EstadoMascota.java
+++ src/State/EstadoMascota.java
@@ -1,7 +1,21 @@
 package State;
 
+/**
+ * Interfaz con los diversos métodos que cambian los estados de la Mascota Virtual.
+ *
+ * @author Supr-Lilito
+ * @author paolasanv
+ * @author JesusEVR
+ * @version noviembre 2023
+ *
+ */
 public interface EstadoMascota{
 
+	/**
+	 * Metodo que cambia el estado del Robot a "Suspendido".
+	 * Lo unico que puede hacer en dicho estado es activarse.
+	 *
+	 */
 	public void alimentar();
 	public void jugar();
 	public void dormir();
