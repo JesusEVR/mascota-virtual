0fdbcdbab2357ca251b84291b5d204ed76eecf7b
Supr-Lilito <124287410+Supr-Lilito@users.noreply.github.com>
2023-11-09 00:38:53 -0600

    Menú de eventos implementado

---
+++ src/Minijuegos/MenuEventos.java
@@ -0,0 +1,111 @@
+package Minijuegos;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class MenuEventos{
+
+    private double puntosHambre;
+    private double puntosEnergia;
+    private double puntosFelicidad;
+    private double dineroObtenido;
+    private ListaJuegos listaEventos;
+    private boolean decidioJugar = true;
+
+    Scanner sc = new Scanner(System.in);
+
+    public MenuEventos(){
+        boolean activo = true;
+        int opcion = 0;
+        while(activo){
+            System.out.println("\n"+" 				->-> E V E N T O S <-<- " + "\n");
+            System.out.println();
+            System.out.println("            -----Dificultadad -----");
+            System.out.println("			1. Fácil");
+            System.out.println("			2. Medio");
+            System.out.println("			3. Difícil");
+            System.out.println("			4. EXTREMO");
+            System.out.println("			0. Salir");
+            System.out.println();
+            System.out.print("Elige una opcion: ");
+        
+            try {
+                String opcionUsuario = sc.nextLine();
+                opcion =  Integer.parseInt(opcionUsuario);
+                if(opcion == 0){
+                    decidioJugar = false;
+                    break;
+                }
+                else if(opcion >-1 && opcion < 5){ 
+                    verEventos(opcion);
+                    System.out.print("      Escribe el número del evento: ");
+                    String codigoEvento = sc.nextLine();
+                    seleccionarEvento(codigoEvento);
+                } else { 
+                    System.out.print("		Por favor, elige una opcion valida: ");
+                }
+            }catch (NumberFormatException ex) {
+                System.out.print("		Por favor, elige una opcion valida: "); 
+            }
+
+        }
+        
+    
+    }
+        
+    public boolean jugado(){
+        return decidioJugar;
+    }
+    
+
+    public double modificaHambre(){
+        return puntosHambre;
+    }
+
+    public double modificaEnergia(){
+        return puntosEnergia;
+    }
+
+    public double modificaFelicidad(){
+        return puntosFelicidad;
+    }
+
+    public double obtenerDinero(){
+        return dineroObtenido;
+    }
+
+
+    public void verEventos(int dificultad){
+            listaEventos = new ListaJuegos();
+            ConstructorEventos con;
+            Iterator i = listaEventos.creaIterador();
+            while(i.hasNext()){
+                con = (ConstructorEventos) i.next();
+                if(con.obtenerDificultad() == dificultad){
+                    System.out.print(" " + con.obtenerCodigo() + "   ");
+                    System.out.println(con.obtenerTitulo());
+                    System.out.println("	" + con.obtenerDescripcion());
+                    System.out.println();
+                }
+            }
+    }
+
+    public void seleccionarEvento(String codigo){
+        ConstructorEventos con;
+        Iterator i = listaEventos.creaIterador();
+
+        while(i.hasNext()){
+            con = (ConstructorEventos) i.next();
+            if(con.obtenerCodigo().equals(codigo)){
+                con.obtenerResultado();
+                dineroObtenido = con.obtenerDinero();
+                puntosHambre = con.modificaHambre();
+                puntosEnergia = con.modificaEnergia();
+                puntosFelicidad = con.modificaFelicidad();
+                System.out.println();
+                return;
+            }
+        }
+        System.out.println("El código es inválido.");
+    }
+}
