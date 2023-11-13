a4f5afb83aae2327e83d07474fd45cb8d0775a8e
Supr-Lilito <124287410+Supr-Lilito@users.noreply.github.com>
2023-11-13 01:36:46 -0600

    documentación realizada

--- src/Minijuegos/Caso.java
+++ src/Minijuegos/Caso.java
@@ -1,13 +1,48 @@
 package Minijuegos;
 
+/**
+ * Clase que define un caso de ocurrencia dentro de un evento.
+ * 
+ * @author paolasanv
+ * @author Supr-Lilito
+ * @author JesusEVR
+ * @version noviembre 2023
+ *
+ */
 public class Caso{
+    /**
+     * texto a imprimir cuando ocurre un caso.
+     */
     private String texto;
+    /**
+     * Cantidad de dinero que se gana al ocurrir un evento
+     */
     private double dineroObtenido;
+    /**
+	* Valor de los puntos que se pierde/gana en el atributo hambre de la mascota
+	*/
     private double puntosHambre;
+    /**
+	* Valor de los puntos que se pierde/gana en el atributo felicidad de la mascota
+	*/
     private double puntosFelicidad;
+    /**
+	* Valor de los puntos que se pierde/gana en el atributo energía de la mascota
+	*/
     private double puntosEnergia;
+    /**
+     * booleano que define por defecto que la mascota no muere
+     */
     private boolean muere = false;
 
+    /**
+     * Método constructor de un caso
+     * @param texto resultado del caso
+     * @param dineroObtenido dinero ganado en el caso
+     * @param puntosHambre puntos de hambre a modificar en la mascota
+     * @param puntosEnergia puntos de energia a modificar en la mascota
+     * @param puntosFelicidad puntos de felicidad a modificar en la mascota
+     */
     public Caso(String texto, double dineroObtenido, double puntosHambre, double puntosEnergia, double puntosFelicidad){
         this.texto = texto;
         this.dineroObtenido = dineroObtenido;
@@ -16,6 +51,15 @@ public class Caso{
         this.puntosEnergia = puntosEnergia;
     }
 
+    /**
+     * Método constructor de un caso donde la mascota muere.
+     * @param texto resultado del caso
+     * @param dineroObtenido dinero ganado en el caso
+     * @param puntosHambre puntos de hambre a modificar en la mascota
+     * @param puntosEnergia puntos de energia a modificar en la mascota
+     * @param puntosFelicidad puntos de felicidad a modificar en la mascota
+     * @param muere si la mascota muere
+     */
     public Caso(String texto, double dineroObtenido, double puntosHambre, double puntosEnergia, double puntosFelicidad, boolean muere){
         this.texto = texto;
         this.dineroObtenido = dineroObtenido;
@@ -25,34 +69,74 @@ public class Caso{
         this.muere = muere;
     }
 
+    /**
+     * Método que asigna el texto de un caso.
+     * @param texto
+     */
     public void asignarTexto(String texto){
         this.texto = texto;
     }
 
+    /**
+     * método que asigna el dinero obtenido de un caso.
+     * @param dineroObtenido
+     */
     public void asignarDineroObtenido(double dineroObtenido){
         this.dineroObtenido = dineroObtenido;
     }
 
+    /**
+     * Método que asigna si la mascota muere durante un evento o caso.
+     * @param muere
+     */
+    public void asignarMuerte(boolean muere){
+        this.muere = muere;
+    }
+
+    /**
+     * Método que devuelve los puntos de Hambre a modificar en la mascota.
+     * @return puntosHambre
+     */
     public double modificaHambre(){
         return puntosHambre;
     }
 
-    public double modificaFelicidad(){
-        return puntosFelicidad;
-    }
-
+    /**
+     * Método que devuelve los puntos de Energía a modificar en la mascota.
+     * @return puntosEnergia
+     */
     public double modificaEnergia(){
         return puntosEnergia;
     }
 
+    /**
+     * Método que devuelve los puntos de Felicidad a modificar en la mascota.
+     * @return puntosFelicidad
+     */
+    public double modificaFelicidad(){
+        return puntosFelicidad;
+    }
+
+    /**
+     * Método que determina si la mascota ha muerto.
+     * @return muere - verdadero si la mascota está muerta, falso si está viva.
+     */
     public boolean obtenerMuerte(){
         return muere;
     }
 
+    /**
+     * Método que devuelve la cantidad de dinero obtenido.
+     * @return dineroObtenido
+     */
     public double obtenerDinero(){
         return dineroObtenido;
     }
 
+    /**
+     * Método que devuelve un string con el texto del resultado.
+     * @return
+     */
     public String textoResultado(){
         return texto;
     }
--- src/Minijuegos/ConstructorEventos.java
+++ src/Minijuegos/ConstructorEventos.java
@@ -2,64 +2,135 @@ package Minijuegos;
 
 import java.util.Random;
 
+/**
+ * Clase que define un evento con resultados de cantidad y probabilidad variable, la cual puede ser ajustada
+ * en su creación.
+ * 
+ * @author paolasanv
+ * @author Supr-Lilito
+ * @author JesusEVR
+ * @version noviembre 2023
+ *
+ */
 public class ConstructorEventos{
+    /**
+     * Código que identifica al evento
+     */
     private String codigo;
+    /**
+     * título del evento
+     */
     private String titulo;
+    /*
+     * Descripción del evento
+     */
     private String descripcion;
+    /**
+     * dificultad para clasificar los eventos según la probabilidad
+     */
     private int dificultad;
+    /**
+     * Casos posibles que pueden ocurrir en un evento
+     */
     public Caso[] casos;
+    /**
+     * Rango de probabilidad que se va a manejar en el evento,
+     * es decir el valor que ocupa el denominador en la probabilidad.
+     * 
+     * Dentro de este arreglo se puede ajustar la amplitud de la probabilidad de cada caso.
+     */
     private Integer[] rangoProb;
+    /**
+	* Valor de los puntos que se pierde/gana en el atributo hambre de la mascota
+	*/
     private double puntosHambre;
+    /**
+	* Valor de los puntos que se pierde/gana en el atributo felicidad de la mascota
+	*/
     private double puntosFelicidad;
+    /**
+	* Valor de los puntos que se pierde/gana en el atributo energía de la mascota
+	*/
     private double puntosEnergia;
+    /**
+     * Cantidad de dinero que se gana al ocurrir un evento
+     */
     private double dineroObtenido;
+    /**
+     * valor para identificar un caso en el rango de probabilidad
+     */
     private int contadorCaso = 0;
-    private int valorResultado;
+    /**
+     * valor que define el tamanio del rango de la probabilidad
+     */
+    private int tamanioProb;
+    /**
+     * Texto del caso resultante
+     */
     private String resultadoTexto;
+    /**
+     * booleano que define si la mascota muere en el evento
+     */
     private boolean muere = false;
     
-
+    /**
+     * Constructor de un Evento.
+     * @param codigo Identificador del evento
+     * @param titulo Titulo del evento
+     * @param descripcion Descripcion del evento
+     * @param dificultad Dificultad del evento
+     * @param numCasos Número de casos posibles
+     * @param rangoProb Rango de la probabilidad asignable a los casos
+     */
     public ConstructorEventos(String codigo, String titulo, String descripcion, int dificultad, int numCasos, int rangoProb){
         this.codigo = codigo;
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.dificultad = dificultad;
         casos = new Caso[numCasos];
-        valorResultado = rangoProb;
+        tamanioProb = rangoProb; 
         this.rangoProb = new Integer[rangoProb];
     }
-/* 
-    public ConstructorEventos(String codigo, String titulo, String descripcion, int dificultad, int numCasos, int rangoProb, boolean muere){
-        this.codigo = codigo;
-        this.titulo = titulo;
-        this.descripcion = descripcion;
-        this.dificultad = dificultad;
-        casos = new Caso[numCasos];
-        valorResultado = rangoProb;
-        this.rangoProb = new Integer[rangoProb];
-        this.muere = muere;
-    }
-    */
 
-    public ConstructorEventos(){
-    }
-
+    /**
+     * Método que asigna los puntos de hambre a modificar en la mascota.
+     * @param puntosHambre
+     */
     public void asignarPuntosHambre(double puntosHambre){
         this.puntosHambre = puntosHambre;
     }
 
+    /**
+     * Método que asigna los puntos de felicidad a modificar en la mascota.
+     * @param puntosFelicidad
+     */
     public void asignarPuntosFelicidad(double puntosFelicidad){
         this.puntosFelicidad = puntosFelicidad;
     }
 
+    /**
+     * Método que asigna los puntos de energía a modificar en la mascota.
+     * @param puntosEnergia
+     */
     public void asignarPuntosEnergia(double puntosEnergia){
         this.puntosEnergia = puntosEnergia;
     }
 
+    /**
+     * Método que asigna el dinero obtenido (pejecoins) de un evento.
+     * @param dineroObtenido
+     */
     public void asignarDineroObtenido(double dineroObtenido){
         this.dineroObtenido = dineroObtenido;
     }
 
+    /**
+     * Método que asigna la probabilidad de un caso dentro del arreglo []rangoProb.
+     * ej: 3/15.
+     * 3 es la amplitud de probabilidad
+     * 15 es el tamaño de []rangoProb
+     * @param amplitudProb probabilidad de ocurrencia de un caso
+     */
     public void asignarAmplitudProb(int amplitudProb){
         int contador = amplitudProb;
         for(int i = 0; i < rangoProb.length; i++){
@@ -68,54 +139,102 @@ public class ConstructorEventos{
                 contador--;
             }
         }
-        contadorCaso++;
+        contadorCaso++; //numero de referencia a un caso dentro de rangoProb
     }
 
+    /**
+     * Método que asigna si la mascota muere durante un evento o caso.
+     * @param muere
+     */
     public void asignarMuerte(boolean muere){
         this.muere = muere;
     }
 
+    /**
+     * Método que devuelve los puntos de Hambre a modificar en la mascota.
+     * @return puntosHambre
+     */
     public double modificaHambre(){
         return puntosHambre;
     }
 
+    /**
+     * Método que devuelve los puntos de Energía a modificar en la mascota.
+     * @return puntosEnergia
+     */
     public double modificaEnergia(){
         return puntosEnergia;
     }
 
+    /**
+     * Método que devuelve los puntos de Felicidad a modificar en la mascota.
+     * @return puntosFelicidad
+     */
     public double modificaFelicidad(){
         return puntosFelicidad;
     }
 
+    /**
+     * Método que determina si la mascota ha muerto.
+     * @return muere - verdadero si la mascota está muerta, falso si está viva.
+     */
     public boolean obtenerMuerte(){
         return muere;
     }
 
+    /**
+     * Método que devuelve la cantidad de dinero obtenido.
+     * @return dineroObtenido
+     */
     public double obtenerDinero(){
         return dineroObtenido;
     }
 
+    /**
+     * Método que devuelve el código asociado.
+     * @return codigo
+     */
     public String obtenerCodigo(){
         return codigo;
     }
 
-
+    /**
+     * Método que devuelve el nivel de dificultad.
+     * @return dificultad
+     */
     public int obtenerDificultad(){
         return dificultad;
     }
 
+    /**
+     * Método que devuelve el título correspondiente.
+     * @return titulo
+     */
     public String obtenerTitulo(){
         return titulo;
     }
 
+    /**
+     * Método que devuelve la descripción asociada.
+     * @return descripcion
+     */
     public String obtenerDescripcion(){
         return descripcion;
     }
 
+    /**
+     * Método que agrega un caso al arreglo de casos, con sus valores definidos.
+     * @param resultado texto del resultado del caso
+     * @param dineroObtenido dinero ganado del caso
+     * @param puntosHambre puntos de Hambre a modificar
+     * @param puntosEnergia puntos de energia a modificar
+     * @param puntosFelicidad puntos dse felicidad a modificar
+     * @param amplitudProb probabilidad de ocurrencia del caso dentro del arreglo []rangoProb
+     */
     public void agregarCaso(String resultado, double dineroObtenido, double puntosHambre, double puntosEnergia, double puntosFelicidad, int amplitudProb){
         for(int i = 0; i < casos.length; i++){
             if(casos[i] == null){
-                if(muere){
+                if(muere){ //se crea un objeto Caso donde se guarda si la mascota muere
                     casos[i] = new Caso(resultado, dineroObtenido, puntosHambre, puntosEnergia, puntosFelicidad, true);
                 }else{
                     casos[i] = new Caso(resultado, dineroObtenido, puntosHambre, puntosEnergia, puntosFelicidad);
@@ -126,9 +245,13 @@ public class ConstructorEventos{
         }
     }
 
+    /**
+     * Método que obtiene al azar el resultado de un evento, y
+     * asigna los valores resultantes del caso ocurrido.
+     */
     public void obtenerResultado(){
         Random r = new Random();
-        int numGanador = r.nextInt(valorResultado);
+        int numGanador = r.nextInt(tamanioProb); //numero que hace referencia a la casilla del arreglo
         int valorCasilla = rangoProb[numGanador];
 
         for(int i = 0; i < casos.length; i++){
@@ -145,8 +268,10 @@ public class ConstructorEventos{
         }
     }
 
-
-
+    /**
+     * Método que devuelve un String con el texto del caso resultante
+     * @return texto del caso resultante
+     */
     public String imprimeResultado(){
         return resultadoTexto;
     }
